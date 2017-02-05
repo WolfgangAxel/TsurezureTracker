a6b21c514772453a112388bb922ff85c43f8c561
Keaton <linux.keaton@gmail.com>
2017-02-05 00:38:58 -0600

    V.0.9.2
    Check main script changes

--- Modules/reddit_fxns.py
+++ Modules/reddit_fxns.py
@@ -39,23 +39,119 @@
 if __name__ == '__main__':
     exit("Not a script")
 
-from __main__ import R_BOT_USER, R_BOT_PASS, R_BOT_C_ID, R_BOT_SCRT, R_BOT_MAST, BATOTO_USR, BATOTO_PWD, characterLookup, Archives, batotoChapters, get, search, TRZRURL, WAKAURL
+import __main__
 import praw
 
-sub = "manga"
+sub = "roboruri"
 mySub = "TsurezureTracker"
 
-botFlair = "Enjoy!\n\n****\n\n[^(I am a bot.)](https://github.com/WolfgangAxel/TsurezureTracker) ^(Request a lookup from me )[^(here,)](https://www.reddit.com/message/compose/?to=TsurezureTracker&subject=Lookup&message=find%20CHARACTER%20CHARACTER%20CHARACTER%20chapters)^( or look through yourself )[^(here.)](http://bato.to/forums/topic/23170-relationship-continuation-list-by-moredrowsy/) ^(Complaints? /r/"+mySub+")"
+botFlair =("Enjoy!\n\n****\n\n[^(I am a bot.)](https://github.com/WolfgangAxel/TsurezureTracker) "+
+           "^(Request a lookup from me )[^(here,)](https://www.reddit.com/message/compose/?to="+__main__.R_BOT_USER+
+           "&subject=Lookup&message=find%20CHARACTER%20CHARACTER%20CHARACTER%20chapters)^( or look through yourself )"+
+           "[^(here.)](http://bato.to/forums/topic/23170-relationship-continuation-list-by-moredrowsy/) "+
+           "^(Complaints? /r/"+mySub+")"
+          )
+adminCommandParsers = {
+    "botHelp(parsed.group())":"help",
+    "__main__.Archives.addMain(parsed.group(1).capitalize(),parsed.group(2).capitalize(),botMode = True)":"add character: (.*) as family name, (.*) as given name",
+    "editChapterReply(parsed.group(1),parsed.group(2),reddit,message)":"edit submission url: (.*) characters \[(.*)\]",
+    "__main__.Archives.addChapter(__main__.characterLookup,parsed.group(1),'* ['+parsed.group(2).title()+']('+parsed.group(3)+')',botMode=True)":
+        "add character (.*) to chapter (.*) at url (.*)",
+    "newSticky(admin=parsed.group())":"new sticky"
+    }
 
 def startup():
+    """
+    Make the reddit instance
+    """
     try:
-        return praw.Reddit(client_id=R_BOT_C_ID,
-                            client_secret=R_BOT_SCRT,
-                            password=R_BOT_PASS,
-                            user_agent="Keeping track of Tsurezure Children chapters for /r/manga, hosted by /u/"+R_BOT_MAST,
-                            username=R_BOT_USER)
+        return praw.Reddit(client_id=__main__.R_BOT_C_ID,
+                            client_secret=__main__.R_BOT_SCRT,
+                            password=__main__.R_BOT_PASS,
+                            user_agent="Keeping track of Tsurezure Children chapters for /r/manga, hosted by /u/"+__main__.R_BOT_MAST,
+                            username=__main__.R_BOT_USER)
     except Exception as e:
         exit("Reddit authentication failed. Check for correct credentials and internet connection\n\nMore details: "+str(e.args()))
+
+def botHelp(_):
+    """
+    Return the functions and the parser for each bot command
+    """
+    helpText = "Here are the admin functions and parsers:\n\n"
+    for command in adminCommandParsers:
+        helpText += "* `"+command+"`: `"+adminCommandParsers[command]+"`\n"
+    helpText += "\n****\n\n"
+    return helpText
+
+def adminFunctions(message,reddit):
+    """
+    Parse a PM from the botmaster with commands
+    """
+    greeting = "Hello, /u/"+__main__.R_BOT_MAST+"! Admin functions engaged.\n\n****\n\n"
+    messageBody = ""
+    for line in message.body.lower().splitlines():
+        for command in adminCommandParsers:
+            # Stack the output from all admin commands into one message, then
+            # send it if there was any successful commands.
+            try:
+                parsed = __main__.search(adminCommandParsers[command],line)
+                output = eval(command)
+                if type(output) == str:
+                    messageBody += output
+                else:
+                    messageBody += "Operation '"+line+"' returned non-string output, but successfully so.\n\n****\n\n"
+            except Exception as e:
+                if not "NoneType" in e.args[0]:
+                    print("Error with "+command+"\n"+str(e.args)+"\n\n")
+                continue
+    if messageBody == "":
+        raise Exception
+    message.reply(greeting+messageBody+botFlair)
+
+def editChapterReply(url,characters,reddit,message):
+    """
+    Admin function to edit the auto-response with the correct characters' chapters
+    """
+    greeting="Hello, /r/"+sub+"!\n\n"
+    # Fucking lovemaster ruining everything
+    characters = characters.replace("love master","lovemaster").split(" ")
+    submission = praw.models.Submission(reddit,url=url)
+    try:
+        parsed = __main__.search("\[DISC\] (Wakabayashi Toshiya's 4-koma Collection|Tsurezure Children)",submission.title).group(1)
+        if parsed.lower() == "tsurezure children":
+            soup = __main__.batotoChapters(__main__.TRZRURL)[0]
+        else:
+            soup = __main__.batotoChapters(__main__.WAKAURL)[0]
+        chapterURL=soup[0].td.a['href']
+        columns = soup[0].find_all('td')
+        name = columns[0].img.next_sibling.strip()
+        chName = __main__.search(".*: (.*)",name).group(1)
+    except:
+        return "Incorrect submission link used.\n\n****\n\n"
+    for comment in submission.comments:
+        if comment.author.name.lower() == __main__.R_BOT_USER.lower():
+            if "There's something I don't understand about this chapter title." in comment.body:
+                try:
+                    for character in characters:
+                        character = __main__.Archives.findNames(character)
+                        __main__.characterLookup = __main__.Archives.addChapter(__main__.characterLookup,character,"* ["+name.replace("[","(").replace("]",")")+"]("+chapterURL+")",botMode=True)
+                    __main__.Archives.saveChapterArchive(__main__.characterLookup)
+                    msgBody = "I've done some digging, and this is what I've found:\n\n" + __main__.Archives.findChapters(characters,__main__.characterLookup)
+                except Exception as e:
+                    return "Error when parsing characters:\n"+str(e.args)+"\n\n****\n\n"
+                retries = 0
+                while True:
+                    try:
+                        comment.edit(greeting+msgBody+botFlair)
+                        newSticky()
+                        return "Comment sucessfully edited.\n\n****\n\n"
+                    except:
+                        print("Comment edit failed. Internet connection error? Trying again in 30 seconds.")
+                        __main__.sleep(30)
+                        retries += 1
+                        if retries > 60:
+                            return "Failed to edit post. Internet issues? If not, IDK what happened...\n\n****\n\n"
+    return "I have not commented in the thread you linked me to\n\n****\n\n"
 
 def checkNewChaps(reddit):
     """
@@ -63,62 +159,90 @@ def checkNewChaps(reddit):
     """
     for submission in reddit.subreddit(sub).new(limit=100):
         try:
-            parsed = search("\[DISC\] (Wakabayashi Toshiya's 4-koma Collection|Tsurezure Children)",submission.title).group(1)
+            parsed = __main__.search("\[DISC\] (Wakabayashi Toshiya's 4-koma Collection|Tsurezure Children)",submission.title).group(1)
         except:
             continue
-        if R_BOT_USER.lower() in [ comment.author.name.lower() for comment in submission.comments ]:
+        if __main__.R_BOT_USER.lower() in [ comment.author.name.lower() for comment in submission.comments ]:
             # Don't comment if we've commented in that thread before
             continue
         if not "bato.to" in submission.url:
             # Gotta be Batoto, since that's where we scrape for characters
             print("Found a discussion thread, but the link wasn't Batoto.")
             continue
-        print("----------\nFound a new chapter\n----------")
+        print(__main__.printTime()+"----------\nFound a new chapter\n----------")
         if parsed.lower() == "tsurezure children":
-            soup = batotoChapters(TRZRURL)[0]
+            soup = __main__.batotoChapters(__main__.TRZRURL)[0]
         else:
-            soup = batotoChapters(WAKAURL)[0]
+            soup = __main__.batotoChapters(__main__.WAKAURL)[0]
+        greeting="Hello, /r/"+sub+"!\n\n"
         chapterURL=soup[0].td.a['href']
         columns = soup[0].find_all('td')
         name = columns[0].img.next_sibling.strip()
-        chName = search(".*: (.*)",name).group(1)
+        chName = __main__.search(".*: (.*)",name).group(1)
         try:
-            characters = Archives.titleParse(chName)
+            characters = __main__.Archives.titleParse(chName)
             for character in characters:
-                characterLookup = addChapter(characterLookup,character,"* ["+name.replace("[","(").replace("]",")")+"]("+chapterURL+")",botMode=True)
-            Archives.saveChapterArchive(characterLookup)
-            previousInteractions = "I've done some digging, and this is what I've found:\n\n" + Archives.findChapters(characters,characterLookup)
+                __main__.characterLookup = __main__.Archives.addChapter(__main__.characterLookup,character,"* ["+name.replace("[","(").replace("]",")")+"]("+chapterURL+")",botMode=True)
+            __main__.Archives.saveChapterArchive(__main__.characterLookup)
+            msgBody = "I've done some digging, and this is what I've found:\n\n" + __main__.Archives.findChapters(characters,__main__.characterLookup)
+            print("Successful parsing. Good job, me!\nMaking new sticky...")
+            newSticky()
         except:
-            msg="There's a new chapter of "+parsed+", but the title parse failed. [Check to see if I need to be called to the thread.]("+submission.shortlink+")"
-            reddit.redditor(R_BOT_MAST).message("Failed to find characters",msg)
-            print("Error with chapter. Message sent")
-            continue
-        submission.reply("Hello, /r/"+sub+"!\n\n"+previousInteractions+botFlair)
-        print("Successful reply. Good job, me!")
+            msgBody="There's something I don't understand about this chapter title. I've notified /u/"+__main__.R_BOT_MAST+", and once he tells me what to do, I'll edit this post with the chapters.\n\n"
+            print("Error with chapter. Reply sent, should edit later")
+        submission.reply(greeting+msgBody+botFlair)
+        print("Reply posted")
 
 def checkInbox(reddit):
+    """
+    Parse PMs and try to respond
+    """
     for message in reddit.inbox.unread():
         try:
             redditor = "/u/"+message.author.name
         except:
             redditor = "/u/"+message.author
-        print("Got a message from "+redditor)
+        print(__main__.printTime()+"Got a message from "+redditor)
+        if redditor.lower() == "/u/"+__main__.R_BOT_MAST.lower():
+            try:
+                adminFunctions(message,reddit)
+                message.mark_read()
+                continue
+            except Exception as e:
+                print("Errors:\n\n"+str(e.args)+"\n\n")
+                print("Admin function call failed. Proceeding with regular lookup.")
         greeting = "Hello "+redditor+"!\n\n"
-        message.mark_read()
         try:
             parser = "find (.*) chapters"
             if message.subject == "username mention":
-                parser = "u/"+R_BOT_USER+" "+parser
-            characters = search(parser.lower(),message.body.lower()).group(1).replace("love master","lovemaster").split(' ')
-            previousInteractions = "I've done some digging, and this is what I've found:\n\n" + Archives.findChapters(characters,characterLookup)
+                parser = "u/"+__main__.R_BOT_USER+" "+parser
+            characters = __main__.search(parser.lower(),message.body.lower()).group(1).replace("love master","lovemaster").split(' ')
+            previousInteractions = "I've done some digging, and this is what I've found:\n\n" + __main__.Archives.findChapters(characters,__main__.characterLookup)
             message.reply(greeting+previousInteractions+botFlair)
             print("Message delivered successfully")
         except:
             print("Message parsing failed")
-            if message.subject == "username mention":
+            if message.subject in ["username mention","comment reply"]:
                 print("Was a mention, no message sent")
+                message.mark_read()
                 continue
             message.reply(greeting+"I was unable to read your message properly. "+
                           "Please check the current archive at /r/"+mySub+" for a list of acceptable names, "+
                           "and list names using only spaces in between like so:\n\n    find Gouda Kamine chapters\n\n"+botFlair)
             print("Failure notification sent")
+        message.mark_read()
+
+def newSticky(admin=False):
+    """
+    Generate a new archive sticky on mySub
+    """
+    mySub = __main__.reddit.subreddit(mySub)
+    # Unset the current sticky
+    try:
+        mySub.sticky().mod.sticky(state=False)
+    except:
+        print("No previous sticky found. Oh well.")
+    new = mySub.submit("Current Archive as of "+__main__.st("%m-%d-%y"),selftext=__main__.printCurrentArchive(botMode=True))
+    new.mod.sticky()
+    if admin:
+        return "New sticky made successfully\n\n****\n\n"
